fa3a8cfc781cabe9796f988fee8946374a85b02f
JeanFraga <JeanFraga@users.noreply.github.com>
2019-11-12 13:36:32 -0500

    added base, twitter.py

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
-from .models import DB
+from decouple import config
+from flask import Flask, render_template, request
+from .models import DB, User
 
 
 # now we make a app factory
@@ -8,12 +9,14 @@ def create_app():
     app = Flask(__name__)
 
     # add our config
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # now have the database know about the app
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return 'Welcome to Twitoff'
+        users = User.query.all()
+        return render_template('base.html', title='Home', users=users)
     return app
