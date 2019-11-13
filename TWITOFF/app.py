ed8bfa463efc5b35a4c68d77f584591ddb70658a
JeanFraga <JeanFraga@users.noreply.github.com>
2019-11-13 14:31:10 -0500

    finished lesson adding compare and forms

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,6 +1,14 @@
+"""
+Build my app factory and do routes and configuration
+"""
+
 from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
+from .predict import predict_user
+from .twitter import add_or_update_user
+from dotenv import load_dotenv
+load_dotenv()
 
 
 # now we make a app factory
@@ -20,6 +28,42 @@ def create_app():
     def root():
         users = User.query.all()
         return render_template('base.html', title='Home', users=users)
+
+    # adding in a new route to add users or get users
+    @app.route('/user', methods=['POST']) # uses form
+    @app.route('/user/<name>', methods=['GET']) # needs parameter
+    def user(name=None, message=''):
+        # add this line in last:
+        name = name or request.values['user_name']
+        #import pdb; pdb.set_trace()
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = "User {} succesfully added!".format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = "Error adding {}: {}".format(name, e)
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets,
+                                message=message)
+
+    # adding in a route for predictions
+    @app.route('/compare', methods=['POST'])
+    def compare(message=''):
+        user1, user2 = sorted([request.values['user1'],
+                               request.values['user2']])
+        if user1 == user2:
+            message = 'Cannot compare a user to themselves'
+        else:
+            prediction = predict_user(user1, user2, request.values['tweet_text'])
+            # prediction = predict_user(user1, user2, request.values['tweet_text'], CACHE)
+            # CACHED_COMPARISONS.add((user1,user2))
+            # CACHE.set('comparisons', dumps(CACHED_COMPARISONS))
+            message = '"{}" is more likely to be said by {} than {}'.format(
+                request.values['tweet_text'], user1 if prediction else user2,
+                user2 if prediction else user1)
+        return render_template('prediction.html', title='Prediction', message=message)
+
 
     @app.route('/reset')
     def reset():
